58556673ce98734fe1afbbd5c2b704f5b183055d
Vincenzo Rivello <vincenzo.rivello@gmail.com>
2010-08-02 11:07:03 +0200

    removed the book resource

--- app/models/book.rb
+++
@@ -1,8 +0,0 @@
-class Book
-  include SimplyStored::Couch
-  
-    property :title  
-    property :name  
-    property :description  
-    property :published , :type => :boolean 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :books
-
   
   
 
